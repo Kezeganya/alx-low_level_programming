1a7c4bfe45faa948ccbffdbefd3c40579d6b8309
kezeganya <kenteezy2014@gmail.com>
2023-08-22 06:31:11 -0700

    add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_square - checks for uppercase character.
+* @size: size
+* Return: void
+*/
+void print_square(int size)
+{
+int i;
+int j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
