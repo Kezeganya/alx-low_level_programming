344a2a98356756167987c347ba729c36259fd8e5
kezeganya <kenteezy2014@gmail.com>
2023-08-25 18:51:40 -0700

    add 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcmp - compares two strings.
+* @s1: string to compare.
+* @s2: string to compare. 
+* Return: returns a negative or positive integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+return strcmp(s1, s2);
+}
